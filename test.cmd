beb211cb25fefcab0fa6217056f5fc94edb9b98c
Rowan Wookey <admin@rwky.net>
2019-07-22 17:07:21 +0100

    Packaging changes
    * Updated npm deps switched to nyc over istanbul
    * Fixed test.cmd/test.sh to use npm run
    * Removed yarn.lock

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 CALL docker run --name mysql-abstraction-mariadb -e MYSQL_ROOT_PASSWORD=root -d -p 127.0.0.1:3307:3306 mariadb:10.1
 CALL timeout 15
-CALL tsc
-CALL mocha --exit --reporter spec --ui tdd tests/*.js --timeout 5000
+CALL npm run build
+CALL npm run coverage
 CALL docker kill mysql-abstraction-mariadb
 CALL docker rm mysql-abstraction-mariadb
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --name mysql-abstraction-mariadb -e MYSQL_ROOT_PASSWORD=root -d -p 127.0.0.1:3307:3306 mariadb:10.1
 sleep 15
-tsc
-mocha --exit --reporter spec --ui tdd tests/*.js --timeout 5000
+npm run build
+npm run coverage 
 docker kill mysql-abstraction-mariadb
 docker rm mysql-abstraction-mariadb
