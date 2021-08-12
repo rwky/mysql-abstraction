97f63fbe5de60ed21e77fcee7236249560efc593
Rowan Wookey <admin@rwky.net>
2021-08-13 00:41:24 +0100

    Updated NPM deps and travis versions

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-docker run --name mysql-abstraction-db -e MYSQL_ROOT_PASSWORD=root -d -p 127.0.0.1:3307:3306 mysql:5.7
+set -eu
+docker network create mysql-abstraction || true
+docker run --net mysql-abstraction --name mysql-abstraction-db -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
 sleep 30
-export MYSQL_USER=root
-export MYSQL_PASSWORD=root
-export MYSQL_HOST=127.0.0.1
-export MYSQL_PORT=3307
-npm test
+docker run \
+    -e MYSQL_USER=root \
+    -e MYSQL_PASSWORD=root \
+    -e MYSQL_HOST=mysql-abstraction-db \
+    --net mysql-abstraction \
+    --rm -ti \
+    -v "$PWD:$PWD" \
+    -w "$PWD" \
+    node:latest npm test
 docker kill mysql-abstraction-db
 docker rm mysql-abstraction-db
